da868f9746c84eb79ad9b2a0bb7b3fc36676f6df
henribru <6639509+henribru@users.noreply.github.com>
2020-04-05 12:58:43 +0200

    Fix return type

--- google-stubs/ads/google_ads/util.pyi
+++ google-stubs/ads/google_ads/util.pyi
@@ -2,8 +2,7 @@ from typing import Any
 
 class ResourceName:
     @classmethod
-    def format_composite(cls, *arg: str):
-        str
+    def format_composite(cls, *arg: str) -> str: ...
 
 def get_nested_attr(obj: Any, attr: str, *args: Any) -> Any: ...
 def convert_upper_case_to_snake_case(string: str) -> str: ...
