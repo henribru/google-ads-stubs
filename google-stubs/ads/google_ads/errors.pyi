db77318eac38997711018013bb342d4a52396bca
henribru <6639509+henribru@users.noreply.github.com>
2020-09-26 16:21:19 +0200

    Regenerate v2 and v3 protos

--- google-stubs/ads/google_ads/errors.pyi
+++ google-stubs/ads/google_ads/errors.pyi
@@ -4,18 +4,15 @@ from google.ads.google_ads.v2.proto.errors.errors_pb2 import (
     GoogleAdsFailure as GoogleAdsFailureV2,
 )
 
-
 class GoogleAdsException(Exception):
     error: grpc.RpcError = ...
     call: grpc.Call = ...
     failure: GoogleAdsFailureV2 = ...
     request_id: str = ...
-
     def __init__(
         self,
         error: grpc.RpcError,
         call: grpc.Call,
         failure: GoogleAdsFailureV2,
         request_id: str,
-    ) -> None:
-        ...
+    ) -> None: ...
