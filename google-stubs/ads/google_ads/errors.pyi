7b0dd6b1262f28cc261764437b6583bea1990ff7
henribru <6639509+henribru@users.noreply.github.com>
2020-09-27 22:27:16 +0200

    Add v3, v4 and v5 to failures

--- google-stubs/ads/google_ads/errors.pyi
+++ google-stubs/ads/google_ads/errors.pyi
@@ -5,16 +5,29 @@ import grpc  # type: ignore
 from google.ads.google_ads.v2.proto.errors.errors_pb2 import (
     GoogleAdsFailure as GoogleAdsFailureV2,
 )
+from google.ads.google_ads.v3.proto.errors.errors_pb2 import (
+    GoogleAdsFailure as GoogleAdsFailureV3,
+)
+from google.ads.google_ads.v4.proto.errors.errors_pb2 import (
+    GoogleAdsFailure as GoogleAdsFailureV4,
+)
+from google.ads.google_ads.v5.proto.errors.errors_pb2 import (
+    GoogleAdsFailure as GoogleAdsFailureV5,
+)
+
+GoogleAdsFailure = Union[
+    GoogleAdsFailureV2, GoogleAdsFailureV3, GoogleAdsFailureV4, GoogleAdsFailureV5
+]
 
 class GoogleAdsException(Exception):
     error: grpc.RpcError = ...
     call: grpc.Call = ...
-    failure: GoogleAdsFailureV2 = ...
+    failure: GoogleAdsFailure = ...
     request_id: str = ...
     def __init__(
         self,
         error: grpc.RpcError,
         call: grpc.Call,
-        failure: GoogleAdsFailureV2,
+        failure: GoogleAdsFailure,
         request_id: str,
     ) -> None: ...
