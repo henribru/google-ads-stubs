c0b0725718a5df505878a500036144be9cd1231f
henribru <6639509+henribru@users.noreply.github.com>
2026-05-18 20:24:31 +0200

    Align interceptor stubs with upstream (#69)
    * Align interceptor stubs with upstream
    
    * Fix formatting
    
    * Fix import
    
    * Ignore invalid overrides

--- google-stubs/ads/googleads/interceptors/interceptor.pyi
+++ google-stubs/ads/googleads/interceptors/interceptor.pyi
@@ -1,39 +1,40 @@
-from typing import Any, NamedTuple
+from dataclasses import dataclass
+from typing import AnyStr, Callable, Sequence
 
 import grpc
+from google.protobuf.message import Message
 
-class _ClientCallDetails(
-    NamedTuple(
-        "_ClientCallDetails",
-        [
-            ("method", str),
-            ("timeout", float | None),
-            ("metadata", tuple[tuple[str, Any], ...] | None),
-            ("credentials", grpc.CallCredentials | None),
-        ],
-    ),
-    grpc.ClientCallDetails,
-): ...
+MetadataType = Sequence[tuple[str, AnyStr]]
+ContinuationType = Callable[[grpc.ClientCallDetails, Message], grpc.Call]
 
 class Interceptor:
+    @dataclass
+    class _ClientCallDetails(grpc.ClientCallDetails):
+        method: str
+        timeout: float | None
+        metadata: MetadataType | None  # type:ignore[assignment]
+        credentials: grpc.CallCredentials | None
+        wait_for_ready: bool | None = ...
+
     @classmethod
     def get_request_id_from_metadata(
-        cls, trailing_metadata: tuple[tuple[str, Any]]
+        cls, trailing_metadata: MetadataType
     ) -> str | None: ...
     @classmethod
-    def parse_metadata_to_json(cls, metadata: list[tuple[str, Any]]) -> str: ...
+    def parse_metadata_to_json(cls, metadata: MetadataType | None) -> str: ...
     @classmethod
-    def format_json_object(cls, obj: Any) -> str: ...
+    def format_json_object(cls, obj: dict) -> str: ...
     @classmethod
     def get_trailing_metadata_from_interceptor_exception(
-        cls, exception: Any
-    ) -> tuple[tuple[str, Any]]: ...
+        cls, exception: grpc.RpcError
+    ) -> MetadataType: ...
     @classmethod
     def get_client_call_details_instance(
         cls,
         method: str,
         timeout: float,
-        metadata: list[tuple[str, Any]],
+        metadata: MetadataType,
         credentials: grpc.CallCredentials | None = None,
-    ) -> _ClientCallDetails: ...
+        wait_for_ready: bool | None = None,
+    ) -> grpc.ClientCallDetails: ...
     def __init__(self, api_version: str) -> None: ...
