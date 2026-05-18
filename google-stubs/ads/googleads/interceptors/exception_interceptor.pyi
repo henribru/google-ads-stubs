7651933a0ccbea296984b7dd7d63651a2cac7875
henribru <6639509+henribru@users.noreply.github.com>
2026-05-18 20:10:40 +0200

    Fix import

--- google-stubs/ads/googleads/interceptors/exception_interceptor.pyi
+++ google-stubs/ads/googleads/interceptors/exception_interceptor.pyi
@@ -5,7 +5,7 @@ from _typeshed import Incomplete
 from google.protobuf.message import Message
 
 from google.ads.googleads.interceptors import ContinuationType, Interceptor
-from google.ads.googleads.response_wrappers import (
+from google.ads.googleads.interceptors.response_wrappers import (
     _UnaryStreamWrapper,
     _UnaryUnaryWrapper,
 )
